9297f45d0d44a56116468e406d6053dd0cf569fb
JUNYONG <dhwnsdyd21@gmail.com>
2025-04-01 19:56:43 +0900

    Add files via upload

---
+++ Practice_R/week_05/week_05_study.R
@@ -0,0 +1,6 @@
+data <- read.csv('/Users/oj/Desktop/Yoo_Lab/Heatlthcaredata_analysis_theory/week05_Hypothesis testing/birthweight.csv')
+#structure,str()로로 데이터구조파악
+str(data)
+
+with(data,table(smoke))
+
